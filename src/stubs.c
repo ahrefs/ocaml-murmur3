c06d6cf2bd6a2b0b144f48c48323682ae58bcc62
Raman Varabets <raman@ahrefs.com>
2017-07-17 12:43:25 +0800

    Set up project, implement basic bindings

---
+++ src/stubs.c
@@ -0,0 +1,25 @@
+
+#include <caml/alloc.h>
+#include <caml/memory.h>
+#include <caml/mlvalues.h>
+
+#include "murmur3.h"
+
+CAMLprim value ml_hash64(value data)
+{
+    CAMLparam1(data);
+    uint64_t out[2];
+    MurmurHash3_x64_128(String_val(data), caml_string_length(data), 0, &out);
+    CAMLreturn(caml_copy_int64(out[0]));
+}
+
+CAMLprim value ml_hash128(value data)
+{
+    CAMLparam1(data);
+    uint64_t out[2];
+    MurmurHash3_x64_128(String_val(data), caml_string_length(data), 0, &out);
+    value result = caml_alloc_tuple(2);
+    Store_field(result, 0, caml_copy_int64(out[0]));
+    Store_field(result, 1, caml_copy_int64(out[1]));
+    CAMLreturn(result);
+}
