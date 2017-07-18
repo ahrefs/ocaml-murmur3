b8c94900bb61140b32317d476d63b9f04def5187
Raman Varabets <raman@ahrefs.com>
2017-07-18 10:34:46 +0800

    fix potential corruption in hash128

--- src/murmur3_stubs.c
+++ src/murmur3_stubs.c
@@ -16,9 +16,10 @@ CAMLprim value caml_murmur3_hash64(value data)
 CAMLprim value caml_murmur3_hash128(value data)
 {
     CAMLparam1(data);
+    CAMLlocal1(result);
     uint64_t out[2];
     MurmurHash3_x64_128(String_val(data), caml_string_length(data), 0, &out);
-    value result = caml_alloc_tuple(2);
+    result = caml_alloc_tuple(2);
     Store_field(result, 0, caml_copy_int64(out[0]));
     Store_field(result, 1, caml_copy_int64(out[1]));
     CAMLreturn(result);
